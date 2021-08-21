be264ff205af0ea883e34c6e895400ea0f02da83
Egor Muratov <egormuratov@yahoo.com>
2021-08-21 12:08:40 +0300

    Refactoring

--- src/test/java/ru/aviasales/tests/ConsoleTests.java
+++ src/test/java/ru/aviasales/tests/ConsoleTests.java
@@ -1,5 +1,9 @@
 package ru.aviasales.tests;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import ru.aviasales.config.Project;
+import ru.aviasales.helpers.CommonSteps;
 import ru.aviasales.helpers.DriverUtils;
 import io.qameta.allure.Description;
 import org.junit.jupiter.api.DisplayName;
@@ -13,13 +17,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ConsoleTests extends TestBase {
 
+    @BeforeAll
+    static void configureBaseUrl() {
+        Configuration.baseUrl = Project.config.baseUrl();
+        new CommonSteps().openPage("");
+    }
+
     @Test
     @Description("Console test")
     @DisplayName("Page console log should not have errors")
     void consoleShouldNotHaveErrorsTest() {
-        step("Open url 'https://aviasales.ru/'", () ->
-            open("https://aviasales.ru/"));
-
         step("Console logs should not contain text 'SEVERE'", () -> {
             String consoleLogs = DriverUtils.getConsoleLogs();
             String errorText = "SEVERE";
