50737b3cb999daafbde33726c478091ca725465a
Egor Muratov <egormuratov@yahoo.com>
2021-08-21 11:51:13 +0300

    the project was created

---
+++ src/test/java/ru/aviasales/tests/ConsoleTests.java
@@ -0,0 +1,30 @@
+package ru.aviasales.tests;
+
+import ru.aviasales.helpers.DriverUtils;
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.title;
+import static io.qameta.allure.Allure.step;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class ConsoleTests extends TestBase {
+
+    @Test
+    @Description("Console test")
+    @DisplayName("Page console log should not have errors")
+    void consoleShouldNotHaveErrorsTest() {
+        step("Open url 'https://aviasales.ru/'", () ->
+            open("https://aviasales.ru/"));
+
+        step("Console logs should not contain text 'SEVERE'", () -> {
+            String consoleLogs = DriverUtils.getConsoleLogs();
+            String errorText = "SEVERE";
+
+            assertThat(consoleLogs).doesNotContain(errorText);
+        });
+    }
+}
