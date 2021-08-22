015ed46467ac227a323e9c44c174141e7cccfab8
Egor Muratov <egormuratov@yahoo.com>
2021-08-22 12:11:15 +0300

    Add Set Cookie method

--- src/test/java/ru/aviasales/helpers/CommonSteps.java
+++ src/test/java/ru/aviasales/helpers/CommonSteps.java
@@ -1,12 +1,24 @@
 package ru.aviasales.helpers;
 
 import io.qameta.allure.Step;
+import org.openqa.selenium.Cookie;
 
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.refresh;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class CommonSteps {
     @Step("Open page {url}")
     public void openPage(String url) {
         open(url);
+    }
+
+    @Step("Set Cookie: {cookieName}: {cookieValue}")
+    public void setCookie(String cookieName, String cookieValue) {
+        getWebDriver().manage().deleteCookie(
+                getWebDriver().manage().getCookieNamed(cookieName)
+        );
+        getWebDriver().manage().addCookie(new Cookie(cookieName, cookieValue));
+        refresh();
     }
 }
